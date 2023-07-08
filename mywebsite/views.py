8fb3d5c7ed453c834387877e8e7d0f0718d4f922
Marco Acea <macea@Marcos-MacBook-Air.local>
2023-07-08 00:37:38 -0400

    Initial commit of Projects page

--- mywebsite/views.py
+++ mywebsite/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 home = lambda request: render(request, "home.html")
 resume = lambda request: render(request, "resume.html")
+projects = lambda request: render(request, "projects.html")
 
 under_construction = lambda request: render(request, "under_construction.html")
 about = under_construction
-projects = under_construction
 contact = under_construction
